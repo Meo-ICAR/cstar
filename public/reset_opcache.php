82f97aab7ac56ecb364e550b67016736e5a9c992
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-02-17 17:32:00 +0100

    cleanup

---
+++ public/reset_opcache.php
@@ -0,0 +1,7 @@
+<?php
+if (function_exists('opcache_reset')) {
+    echo opcache_reset() ? 'OpCache reset successful' : 'OpCache reset failed';
+} else {
+    echo 'OpCache is not enabled or function not available';
+}
+unlink(__FILE__);
