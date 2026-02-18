3ce7a4db8f94218f71dbaca1057dfa3383f2d9d7
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-02-18 14:07:35 +0100

    rimozione_file_inutili

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,20 +8,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;  // <--- Per Breezy
 use Jeffgreco13\FilamentBreezy\Traits\TwoFactorAuthenticatable;
-use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;  // <--- Per Filament Shield
 // 1. IMPORTA QUESTE CLASSI
+use Spatie\Activitylog\LogOptions;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
     // <--- USA IL TRAIT
     use HasRoles;
     use LogsActivity;
-
     // Aggiungi questi Trait
     use TwoFactorAuthenticatable;
 
@@ -29,18 +27,19 @@ class User extends Authenticatable
      * 3. AGGIUNGI QUESTO METODO
      * Questo decide chi può vedere l'admin panel in produzione.
      */
-    public function canAccessPanel(Panel $panel): bool
-    {
-        // Se sei in locale, fai entrare tutti (comodo per test)
-        if (app()->environment('local')) {
-            return true;
-        }
-
-        // In Staging/Produzione:
-        // Entra SOLO se ha il ruolo super_admin OPPURE se l'email è quella specifica
-        return $this->hasRole('super_admin');
-    }
-
+    /*
+     * public function canAccessPanel(Panel $panel): bool
+     * {
+     *     // Se sei in locale, fai entrare tutti (comodo per test)
+     *     if (app()->environment('local')) {
+     *         return true;
+     *     }
+     *
+     *     // In Staging/Produzione:
+     *     // Entra SOLO se ha il ruolo super_admin OPPURE se l'email è quella specifica
+     *     return $this->hasRole('super_admin');
+     * }
+     */
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
